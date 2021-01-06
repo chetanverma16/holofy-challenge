27a4065723ca01430ba1118f1e8a8746bdbd920c
Chetan Verma <chetan61verma@gmail.com>
2021-01-06 14:44:48 +0530

    Holofy Challenge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Scene from "./video/scene.mp4";
+import Draggable from "react-draggable";
 
 function App() {
+  const [pos, setPos] = useState({ x: 0, y: -50 });
+  const onStop = (e, data) => {
+    const halfWidth = window.innerWidth / 2;
+    const halfHeight = window.innerHeight / 2;
+
+    if (data.x < halfWidth && data.y < halfHeight) {
+      setPos({ x: 0, y: -50 });
+    } else if (data.x > halfWidth && data.y < halfHeight) {
+      setPos({ x: window.innerWidth - 250, y: -50 });
+    } else if (data.x < halfWidth && data.y > halfHeight) {
+      setPos({ x: 0, y: window.innerHeight - 200 });
+    } else if (data.x > halfWidth && data.y > halfHeight) {
+      setPos({ x: window.innerWidth - 250, y: window.innerHeight - 200 });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>HOLOFY CHALLENGE</h1>
+      </div>
+      <Draggable
+        handle=".handle"
+        defaultPosition={pos}
+        position={pos}
+        scale={1}
+        onStop={onStop}>
+        <div>
+          <div className="handle">
+            <video src={Scene} autoPlay={true}></video>
+          </div>
+        </div>
+      </Draggable>
+    </>
   );
 }
 
