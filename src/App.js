592cd42c1a90b838a6c77580d3eba855b741e617
Chetan Verma <chetan61verma@gmail.com>
2021-01-11 19:06:31 +0530

    Changes for movile and desktop

--- src/App.js
+++ src/App.js
@@ -1,64 +1,115 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Scene from "./video/scene.mp4";
 
 function App() {
   const [pos, setPos] = useState({ x: 0, y: 0 });
-  const [holding, setHolding] = useState(false);
+  const [style, setStyle] = useState({});
+  const [dragging, setDragging] = useState(false);
   const [elem, setElem] = useState(React.createRef());
   const [isPause, setIsPause] = useState(true);
-  // const onStop = (e, data) => {
-  //   const halfWidth = window.innerWidth / 2;
-  //   const halfHeight = window.innerHeight / 2;
 
-  //   if (data.x < halfWidth && data.y < halfHeight) {
-  //     setPos({ x: 0, y: -50 });
-  //   } else if (data.x > halfWidth && data.y < halfHeight) {
-  //     setPos({ x: window.innerWidth - 250, y: -50 });
-  //   } else if (data.x < halfWidth && data.y > halfHeight) {
-  //     setPos({ x: 0, y: window.innerHeight - 200 });
-  //   } else if (data.x > halfWidth && data.y > halfHeight) {
-  //     setPos({ x: window.innerWidth - 250, y: window.innerHeight - 200 });
-  //   }
-  // };
-
-  useEffect(() => {
-    window.addEventListener("mousemove", handleMove);
-    window.addEventListener("mouseleave", notHoldingElem);
-  }, [holding]);
-
-  const notHoldingElem = (e) => {
+  const handleDragStart = (e) => {
     e.preventDefault();
-    setHolding(false);
+    setPos({
+      x: e.clientX - e.currentTarget.getBoundingClientRect().left,
+      y: e.clientY - e.currentTarget.getBoundingClientRect().top,
+    });
+    setDragging(true);
   };
-
-  const holdingElem = (e) => {
+  const handleDragging = (e) => {
     e.preventDefault();
-    setHolding(true);
-  };
+    if (dragging) {
+      var left = e.clientX - pos.x;
+      var top = e.clientY - pos.y;
 
-  const handleMove = (e) => {
+      setStyle({
+        left: left,
+        top: top,
+      });
+    }
+  };
+  const handleDragEnd = (e) => {
+    e.preventDefault();
+    setDragging(false);
     const halfWidth = window.innerWidth / 2;
     const halfHeight = window.innerHeight / 2;
+
+    const currentX = e.clientX;
+    const currentY = e.clientY;
+    if (currentX < halfWidth && currentY < halfHeight) {
+      setStyle({
+        left: 0,
+        top: 0,
+      });
+    } else if (currentX > halfWidth && currentY < halfHeight) {
+      setStyle({
+        left: window.innerWidth - 250,
+        top: 0,
+      });
+    } else if (currentX < halfWidth && currentY > halfHeight) {
+      setStyle({
+        left: 0,
+        top: window.innerHeight - 150,
+      });
+    } else if (currentX > halfWidth && currentY > halfHeight) {
+      setStyle({
+        left: window.innerWidth - 250,
+        top: window.innerHeight - 150,
+      });
+    }
+  };
+
+  const handleTouchDragStart = (e) => {
     e.preventDefault();
-    if (holding) {
-      setPos({ x: e.clientX, y: e.clientY });
-      elem.current.pause();
-      setIsPause(true);
-    } else {
-      const currentX = e.clientX;
-      const currentY = e.clientY;
-      if (currentX < halfWidth && currentY < halfHeight) {
-        setPos({ x: 0, y: 0 });
-      } else if (currentX > halfWidth && currentY < halfHeight) {
-        setPos({ x: window.innerWidth - 250, y: 0 });
-      } else if (currentX < halfWidth && currentY > halfHeight) {
-        setPos({ x: 0, y: window.innerHeight - 150 });
-      } else if (currentX > halfWidth && currentY > halfHeight) {
-        setPos({ x: window.innerWidth - 250, y: window.innerHeight - 150 });
-      }
-      elem.current.play();
-      setIsPause(false);
+    setPos({
+      x: e.touches[0].clientX - e.target.getBoundingClientRect().left,
+      y: e.touches[0].clientY - e.target.getBoundingClientRect().top,
+    });
+    setDragging(true);
+  };
+
+  const handleTouchDragging = (e) => {
+    e.preventDefault();
+    if (dragging) {
+      var left = e.touches[0].clientX - pos.x;
+      var top = e.touches[0].clientY - pos.y;
+      setStyle({
+        left: left,
+        top: top,
+      });
+    }
+  };
+
+  const handleTouchDragEnd = (e) => {
+    console.log(e);
+    e.preventDefault();
+    setDragging(false);
+    const halfWidth = window.innerWidth / 2;
+    const halfHeight = window.innerHeight / 2;
+
+    const currentX = e.changedTouches[0].clientX;
+    const currentY = e.changedTouches[0].clientY;
+    if (currentX < halfWidth && currentY < halfHeight) {
+      setStyle({
+        left: 0,
+        top: 0,
+      });
+    } else if (currentX > halfWidth && currentY < halfHeight) {
+      setStyle({
+        left: window.innerWidth - 200,
+        top: 0,
+      });
+    } else if (currentX < halfWidth && currentY > halfHeight) {
+      setStyle({
+        left: 0,
+        top: window.innerHeight - 200,
+      });
+    } else if (currentX > halfWidth && currentY > halfHeight) {
+      setStyle({
+        left: window.innerWidth - 200,
+        top: window.innerHeight - 200,
+      });
     }
   };
 
@@ -79,12 +130,21 @@ function App() {
       </div>
       <video
         ref={elem}
-        style={{ top: pos.y, left: pos.x }}
         src={Scene}
+        style={style}
         autoPlay={true}
-        onMouseDown={holdingElem}
         onClick={clickHandler}
-        onMouseUp={notHoldingElem}></video>
+        onMouseDown={handleDragStart}
+        onMouseMove={handleDragging}
+        onMouseUp={handleDragEnd}></video>
+
+      <img
+        style={style}
+        onTouchStart={handleTouchDragStart}
+        onTouchMove={handleTouchDragging}
+        onTouchEnd={handleTouchDragEnd}
+        alt="Replacing Video"
+        src="https://images.unsplash.com/photo-1494438639946-1ebd1d20bf85?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80"></img>
     </>
   );
 }
